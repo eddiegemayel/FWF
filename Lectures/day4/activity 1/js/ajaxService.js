bf23115936aa0ba6c4ff70bc294f32659a9278b6
eddiegemayel <eddiegem16@gmail.com>
2014-04-14 13:07:04 -0400

    UPDATE
    GOT THE DATA TO APPEAR IN THE LIST VIEW!!. but nothing else works at
    the moment. at least got the damn json file to load locally on mamp

--- Lectures/day4/activity 1/js/ajaxService.js
+++ Lectures/day4/activity 1/js/ajaxService.js
@@ -7,7 +7,7 @@ angular.module("NewApp").service("MLPService", function($http){
 		if(theData.length === 0){
 			$http.jsonp("http://wddbs.com/ffw/jsapi/mlp.php?callback=JSON_CALLBACK")
 				.success(function(data, status, headers, config){
-					 // console.log(data);
+					  console.log(data);
 					 for(var i =0; i < data.ponies.length; i++){
 					 	theData.push(data.ponies[i]);
 					 }
--- Week 2 Lab/js/ajaxService.js
+++ Week 2 Lab/js/ajaxService.js
@@ -3,12 +3,12 @@ angular.module("CarApp").service("carService", function($http){
 	var cars = [];
 
 	this.getCars = function(){
-		if(cars.length === 0){
+		if(cars.length !== undefined){
 			$http.get("js/json/data.json")
 				.success(function(data, status, headers, config){
 					 console.log(data);
-					 for(var i =0; i < data.vehicle.length; i++){
-					 	cars.push(data.vehicle[i]);
+					 for(var i =0; i < data.vehicles.length; i++){
+					 	cars.push(data.vehicles[i]);
 					 }
 
 				})
