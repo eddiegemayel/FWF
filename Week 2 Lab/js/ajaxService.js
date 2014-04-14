9ea1e930b224fd681c62028ee04583c47cb3fced
eddiegemayel <eddiegem16@gmail.com>
2014-04-14 13:26:16 -0400

    every things basically working
    Add car and Delete car now not only delete and add cars to the json
    list, they also store locally in local storage. just need to make it
    pretty now it seems, will update if anything else needs fixing or
    becomes broken

--- Week 2 Lab/js/ajaxService.js
+++ Week 2 Lab/js/ajaxService.js
@@ -1,17 +1,29 @@
 angular.module("CarApp").service("carService", function($http){
 
+	//empt array to store json data in
 	var cars = [];
 
 	this.getCars = function(){
+
+		//not sure if this is supposed to be here
+		var carList =  localStorage.getItem("carList");
+        cars = JSON.parse(carList) || cars;
+
+
 		if(cars.length === 0){
+			//fetch the json data
 			$http.get("js/json/data.json")
+			//if the file is there, do this
 				.success(function(data, status, headers, config){
+					//console log the data to test, push the data into the empty cars array
 					 console.log(data);
 					 for(var i =0; i < data.vehicles.length; i++){
 					 	cars.push(data.vehicles[i]);
 					 }
 
+
 				})
+				//if the file isnt there do this
 				.error(function(data, status, headers, config){
 					console.log("It broke");
 				})
@@ -27,6 +39,18 @@ angular.module("CarApp").service("carService", function($http){
 		else{
 			return {};
 		}
+	};
+
+	this.deleteCar = function(deadindex){
+		cars.splice(deadindex,1);
+		localStorage.setItem("carList", JSON.stringify(cars));
+	};
+
+	this.addCar = function(car){
+		cars.push(car);
+        localStorage.setItem("carList", JSON.stringify(cars));
+        
+    	document.location.hash = "#/list";
 	};
 
 });
